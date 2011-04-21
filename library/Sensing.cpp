a87b9ec194a8d97cc0957a56b9c4a0f62a761cb7
Ian Cole <ian.cole@gmail.com>
2011-04-20 21:59:59 -0400

    Merging in sirwolfgang's packet nested class with original string-based library code. Packet class NOT used yet. Signed-off-by: Ian Cole <ian.cole@gmail.com>

--- library/Sensing.cpp
+++ library/Sensing.cpp
@@ -1,3 +1,298 @@
+/*
+	Sensing.cpp - Library for the Sensing Platform
+	Created as part of the familab (www.familab.org) entry
+	for the 2011 Greater Global Hackerspace Challenge sponsored by element14
+
+	Copyright (c) 2011 [TODO: OFFICIAL NAME]
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+
+
+*/
+
+/*
+	TODO:
+	 - Addressing - 3pin?
+	 - Use struct or class for packet variables / functions - extensible...
+
+	 - Future: VARIABLE PROTOCOL: Nodes can have more than one sensor reading;
+	 								Each sensor can define the length of its reading
+*/
+
+
+
+
+#include "Sensing.h"
+
+
+class SensingNetWrapper
+{
+	//this class is needed to handle the callback from the wire library to a member function.
+	//wrapper implementation from http://www.comp.ua.ac.be/publications/files/Adapter-Para04.pdf
+
+
+	public:
+		static void onQuery() { return fObj->onQuery(); }
+		static void setObj (SensingNet& obj) {fObj = &obj; }
+
+	private:
+		static SensingNet* fObj;
+
+
+
+};
+
+SensingNet* SensingNetWrapper::fObj=NULL;
+
+
+
+SensingNet::SensingNet()
+{
+	//default constructor
+}
+
+
+
+void SensingNet::begin()
+{
+	//to be called by hub
+	hardWire.begin();
+}
+
+void SensingNet::begin(int addr)
+{
+	//to be called by nodes
+
+	hardWire.begin(addr);
+
+	//wrapper implementation (see declaration above)
+	SensingNetWrapper::setObj(*this);
+	hardWire.onRequest(&SensingNetWrapper::onQuery);
+}
+
+boolean SensingNet::updateNodeData(int id)
+{
+	int bytesRecv = 0;
+
+	hardWire.requestFrom(id, PACKET_LEN);    // request bytes from node
+
+	while(hardWire.available())    // slave may send less than requested
+	{
+	  char c = hardWire.receive(); // receive a byte as character
+
+	  xmitPacket[bytesRecv] = c;
+	  bytesRecv++;
+	}
+
+	if (bytesRecv > 0)
+	{
+
+
+		/*
+		Serial.println("[updateNoteData]");
+		Serial.print("[");
+		Serial.print(id);
+		Serial.print("]");
+		Serial.print("[xmitPacket]:");
+		Serial.println(xmitPacket);
+		*/
+
+		sensorReading = "";
+		sensorUnit="";
+		sensorStatus="";
+
+		String xmitString(xmitPacket);
+		sensorReading = xmitString.substring(0,FIELD1_LEN -1);
+		//Serial.print("[und-sr]:");
+		//Serial.println(sensorReading);
+		sensorUnit = xmitString.substring(FIELD1_LEN,FIELD1_LEN + FIELD2_LEN - 1);
+		//Serial.print("[und-su]:");
+		//Serial.println(sensorUnit);
+		sensorStatus = xmitString.substring(FIELD1_LEN + FIELD2_LEN,FIELD1_LEN + FIELD2_LEN + FIELD3_LEN - 1);
+		//Serial.print("[und-ss]:");
+		//Serial.println(sensorStatus);
+
+		return 0; //recv data
+
+	 }
+
+	 return 1; //no data
+
+
+}
+
+
+
+void SensingNet::setSensorReading(float val, int precision)
+{
+
+
+
+
+	//found float to stringcode here:
+	//http://code.google.com/p/arduino/issues/detail?id=372
+
+	String resultString = "";
+	  // Handle negative numbers
+	  if (val < 0.0)
+	  {
+	     resultString += "-";
+	     val = -val;
+	  }
+
+	  // Round correctly so that print(1.999, 2) prints as "2.00"
+	  double rounding = 0.5;
+	  for (uint8_t i=0; i<precision; ++i)
+	    rounding /= 10.0;
+
+	  val += rounding;
+
+	  // Extract the integer part of the number and print it
+	  unsigned long int_part = (unsigned long)val;
+	  double remainder = val - (double)int_part;
+	  resultString += int_part;
+
+	  // Print the decimal point, but only if there are digits beyond
+	  if (precision > 0)
+	    resultString += ".";
+
+	  // Extract digits from the remainder one at a time
+	  while (precision-- > 0)
+	  {
+	    remainder *= 10.0;
+	    int toPrint = int(remainder);
+	    resultString += toPrint;
+	    remainder -= toPrint;
+	  }
+
+	sensorReading = "";
+	sensorReading.concat(padString(resultString, FIELD1_LEN, ' '));
+
+
+
+	Serial.print("[ssr]:");
+	Serial.println(sensorReading);
+
+    //resultString.toCharArray(sensorReading, FIELD1_LEN);
+
+}
+
+
+
+void SensingNet::setSensorUnit(String unit)
+{
+	sensorUnit = "";
+	sensorUnit.concat(padString(unit, FIELD2_LEN, ' '));
+	Serial.print("[ssu]:");
+	Serial.println(sensorUnit);
+
+}
+
+void SensingNet::setSensorStatus(String status)
+{
+	sensorStatus = "";
+	sensorStatus.concat(padString(status, FIELD3_LEN, ' '));
+	Serial.print("[sss]:");
+	Serial.println(sensorStatus);
+
+}
+
+
+String SensingNet::getSensorReading()
+{
+	//Serial.print("[gsr]:");
+	//Serial.println(sensorReading);
+
+	return sensorReading;
+	//can i just inline these?
+}
+
+String SensingNet::getSensorUnit()
+{
+	//Serial.print("[gsu]:");
+	//Serial.println(sensorUnit);
+	return sensorUnit;
+	//can i just inline these?
+}
+
+String SensingNet::getSensorStatus()
+{
+	//Serial.print("[gss]:");
+	//Serial.println(sensorStatus);
+	return sensorStatus;
+	//can i just inline these?
+}
+
+
+void SensingNet::onQuery() //callback function for slaves to respond when called by the master
+{
+
+	Serial.println("[onQuery]");
+
+	//build xmitPacket
+
+	String xmitString="";
+	xmitString.concat(sensorReading);
+	xmitString.concat(sensorUnit);
+	xmitString.concat(sensorStatus);
+	Serial.print("[xmitString]");
+	Serial.println(xmitString);
+
+	char xmitChar[PACKET_LEN+1];
+	xmitString.toCharArray(xmitChar, PACKET_LEN);
+
+	Serial.print("[xmitChar]");
+	Serial.println(xmitChar);
+
+	hardWire.send(xmitChar); //doesnt xmit the null terminator byte...
+
+
+
+}
+
+
+String SensingNet::padString(String in, int len, char pad)
+{
+	//todo: add left / right pad?
+
+	String result="";
+
+	int padAmount = len - in.length();
+
+	//Serial.print("[pad]:");
+	//Serial.println(padAmount);
+
+	if (padAmount <1) return in;
+
+	else
+	{
+		result.concat(in);
+		for (int i=0; i<padAmount; i++)
+		{
+			result.concat(pad);
+		}
+	}
+
+	return result;
+}
+
 
 SensingNet::Packet::Packet()
 {
@@ -38,6 +333,7 @@ SensingNet::Packet::Packet( const Packet& that )
 		m_sensorStatus[i] = that.m_sensorStatus[i];
 }
 
+/*
 SensingNet::Packet::operator=( Packet that )
 {
 	for (unsigned int i = 0; i < FIELD1_LEN +1; ++i)
@@ -49,6 +345,7 @@ SensingNet::Packet::operator=( Packet that )
 	for (unsigned int i = 0; i < FIELD3_LEN +1; ++i)
 		m_sensorStatus[i] = that.m_sensorStatus[i];
 }
+*/
 
 void SensingNet::Packet::ParsePacket( const char* packet )
 {
--- library/Sensing.h
+++ library/Sensing.h
@@ -1,36 +1,35 @@
 /*
-Sensing.h - Library for the Sensing Platform
+	Sensing.h - Library for the Sensing Platform
+	Created as part of the familab (www.familab.org) entry
+	for the 2011 Greater Global Hackerspace Challenge sponsored by element14
 
-Created as part of the familab (www.familab.org) entry
-for the 2011 Greater Global Hackerspace Challenge sponsored by element14
+	Copyright (c) 2011 Greater Orlando Hackerspaces, Inc.
 
-Copyright (c) 2011 Greater Orlando Hackerspaces, Inc.
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
 
 */
 
 #ifndef SensingNet_h
 #define SensingNet_h
 
-#include "WProgram.h"		//required by arduino libraries
-#include "..\Wire\Wire.h"	//i2c library
+#include "WProgram.h"	//required by arduino libraries
+#include "..\Wire\Wire.h"		//i2c library
 
 #define PACKET_LEN 25
 
@@ -39,77 +38,125 @@ THE SOFTWARE.
 #define FIELD3_LEN 10 //status
 
 /*
-Current Protocol Definition
+	Current Protocol Definition
 
-8 bit characters
+	8 bit characters
 
-Field1: char[10]: Sensor Reading - can be sent in any format,
-but must be padded on the wire
-Field2: char[ 5]: Unit - can be sent in any format,
-but must be padded on the wire
-Field3: char[10]: Status - can be sent in any format,
-but must be padded on the wire
+	Field1: char[10]: Sensor Reading - can be sent in any format,
+										but must be padded on the wire
+	Field2: char[ 5]: Unit - can be sent in any format,
+										but must be padded on the wire
+	Field3: char[10]: Status - can be sent in any format,
+										but must be padded on the wire
 
-Yes, this is overly simplistic, and yet verbose. Our intent is to keep this
-as simple as possible for new students / makers.
-If they want higher performance, they can use the wire library
-directly with a custom protocol.
+	Yes, this is overly simplistic, and yet verbose. Our intent is to keep this
+	as simple as possible for new students / makers.
+	If they want higher performance, they can use the wire library
+	directly with a custom protocol.
 
-Example transmission:
+	Example transmission:
 
-0001.23400m/s^2     Ready
-byte: 0123456789012340123456789
-<-field1-><f2-><-field3->
+	      0001.23400m/s^2     Ready
+	byte: 0123456789012340123456789
+          <-field1-><f2-><-field3->
 
-Example transmission:
+	Example transmission:
 
-123     F
-byte: 0123456789012340123456789
-<-field1-><f2-><-field3->
+	             123     F
+	byte: 0123456789012340123456789
+          <-field1-><f2-><-field3->
 */
 
 class SensingNet
 {
-		class Packet{
 
-			// +1 to allow for Null Terminator
-			char m_sensorReading[FIELD1_LEN +1];
-			char m_sensorUnit[FIELD2_LEN +1];
-			char m_sensorStatus[FIELD3_LEN +1];
 
-		public:
-			// Default Constructor
-			Packet();
+	//NESTED CLASS, used to minimize difficulty in changing protocol later
 
-			//Destructor
-			virtual ~Packet(){}
+	class Packet{
 
-			// Constructor: Parse raw packet into packet
-			Packet(const char* packet){ ParsePacket(packet); }
+				// +1 to allow for Null Terminator
+				char m_sensorReading[FIELD1_LEN +1];
+				char m_sensorUnit[FIELD2_LEN +1];
+				char m_sensorStatus[FIELD3_LEN +1];
 
-			// Constructor: Parse raw data into packet
-			Packet(const char* reading, const char* unit, const char* status);
+			public:
+				// Default Constructor
+				Packet();
 
-			// Copy Constructor
-			Packet(const Packet& that);
+				//Destructor
+				virtual ~Packet(){}
 
-			// Operator= Overload
-			Packet operator=( Packet that);
+				// Constructor: Parse raw packet into packet
+				Packet(const char* packet){ ParsePacket(packet); }
 
-			// Parse raw packet into packet
-			void ParsePacket(const char* packet);
-			void SetReading(const char* data);
-			void SetUnit(const char* unit);
-			void SetStatus(const char* status);
+				// Constructor: Parse raw data into packet
+				Packet(const char* reading, const char* unit, const char* status);
 
-			// Return raw packet
-			char* GetPacket();
-			char* GetReading(){ return m_sensorReading; }
-			char* GetUnit(){ return m_sensorUnit; }
-			char* GetStatus(){ return m_sensorStatus; }
+				// Copy Constructor
+				Packet(const Packet& that);
+
+				// Operator= Overload
+				//Packet operator=( Packet that);
+
+				// Parse raw packet into packet
+				void ParsePacket(const char* packet);
+				void SetReading(const char* data);
+				void SetUnit(const char* unit);
+				void SetStatus(const char* status);
+
+				// Return raw packet
+				char* GetPacket();
+				char* GetReading(){ return m_sensorReading; }
+				char* GetUnit(){ return m_sensorUnit; }
+				char* GetStatus(){ return m_sensorStatus; }
 		};
-		public:
-			// TODO: Everything
-	};
+
+
+
+
+
+
+
+	public:
+		SensingNet(); 				//constructor for master (hub) device
+		void begin();				//match the wire convention - no param = master
+		void begin(int addr);			//param = slave with id
+		void begin(int, int, int);   //params = pins for address detection
+
+		boolean updateNodeData(int);
+
+		void setSensorReading(float, int); //float and double are the same on arduino...
+		//todo: overloaded versions for other datatypes? Needed?
+
+		void setSensorUnit(String unit);
+
+		void setSensorStatus(String status);
+
+		String  getSensorReading();
+		String  getSensorUnit();			//todo:inline these!
+		String  getSensorStatus();
+
+		void onQuery();
+
+		String padString(String in, int len, char pad);
+
+	private:
+
+		String sensorReading;
+		String sensorUnit;
+		String sensorStatus;
+
+		char xmitPacket[PACKET_LEN + 1];
+
+		TwoWire hardWire;
+
+
+
+
+
+};
+
+
 
 #endif
